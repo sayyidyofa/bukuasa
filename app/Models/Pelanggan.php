8a52d3afe561ec0cb82adf8c6179d2c7cd9857b5
sayyidyofa <sayyidyofa@gmail.com>
2020-12-27 20:22:31 +0700

    Add ide-helper

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -7,6 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use \DateTimeInterface;
 
+/**
+ * App\Models\Pelanggan
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $address
+ * @property string|null $contact
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Pembayaran[] $customerPembayarans
+ * @property-read int|null $customer_pembayarans_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Pelanggan onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereContact($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Pelanggan whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Pelanggan withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Pelanggan withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Pelanggan extends Model
 {
     use SoftDeletes, HasFactory;
--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -7,6 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use \DateTimeInterface;
 
+/**
+ * App\Models\ProductCategory
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory newQuery()
+ * @method static \Illuminate\Database\Query\Builder|ProductCategory onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|ProductCategory withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|ProductCategory withoutTrashed()
+ * @mixin \Eloquent
+ */
 class ProductCategory extends Model
 {
     use SoftDeletes, HasFactory;
