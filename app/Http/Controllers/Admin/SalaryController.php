fd84c6e72ebb22f0ceb39dc0a1788c7a9a21ab11
Sayyid Yofa <sayyidyofa@gmail.com>
2021-01-01 17:25:08 +0700

    Add weekly salary form controller

--- app/Http/Controllers/Admin/SalaryController.php
+++ app/Http/Controllers/Admin/SalaryController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MassDestroySalaryRequest;
+use App\Http\Requests\MonthlySalaryFormRequest;
+use App\Http\Requests\MonthlySalaryProcessRequest;
 use App\Http\Requests\StoreSalaryRequest;
 use App\Http\Requests\UpdateSalaryRequest;
+use App\Http\Requests\WeeklySalaryFormRequest;
+use App\Http\Requests\WeeklySalaryProcessRequest;
+use App\Models\Attendance;
+use App\Models\Product;
 use App\Models\Salary;
+use App\Models\SalaryConstant;
 use App\Models\User;
+use App\Models\Welding;
 use Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\Response;
 
 class SalaryController extends Controller
@@ -80,5 +89,66 @@ class SalaryController extends Controller
         Salary::whereIn('id', request('ids'))->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function weeklySalaryForm(WeeklySalaryFormRequest $request) {
+
+        $startDate = $request->get('start_date');
+        $endDate = $request->get('end_date');
+
+        $weldings = Welding::whereBetween('date', [$startDate, $endDate])
+            ->get(['user_id', 'amount_unit', 'weight_kg', 'product_id'])
+            ->groupBy('user_id')->transform(fn($perUserId) => $perUserId->groupBy('product_id'))
+            ->each(fn($perUserAgain) => $perUserAgain
+                ->transform(fn(Collection $perProductId, int $index) => $perProductId->sum->amount_unit*Product::whereId($index)->first()->rate_keping))
+            ->transform(fn(Collection $perUserYetAgain, int $index) => $perUserYetAgain->sum()
+            );
+
+        $otherWeekly = Attendance::whereStatus('hadir')
+            ->select(['id', 'user_id'])
+            ->with(['user' => fn($query) => $query->select('id'), 'user.roles' => fn($query) => $query->select(['id', 'title'])])
+            ->whereBetween('date', [$startDate, $endDate])
+            ->get()
+            ->groupBy(fn($item) => $item->user->roles->first()->title)
+            ->forget(config('roles.welder'))
+            ->transform(fn($cole) => $cole->groupBy('user_id'))
+            ->each(fn ($colee) => $colee
+                ->transform(fn ($coleee) =>
+                    $coleee->count() * $colee->first()->first()->user->salaryConstant()
+                    + ($coleee->count() === 6 && $colee->first()->first()->user->salaryConstant() <= SalaryConstant::where('name', '=', 'Bare Minimum Bonus Kehadiran')->get()->first()->nominal
+                        ? SalaryConstant::where('name', '=', 'Bonus Kehadiran')->get()->first()->nominal
+                        : 0
+                    )
+                )
+            );
+
+        $otherWeekly->put('Operator Las', $weldings);
+
+        $viewData = $otherWeekly;
+
+        return view('admin.salaries.form.weekly', compact(
+            'viewData', 'startDate', 'endDate'
+        ));
+    }
+
+    public function monthlySalaryForm(MonthlySalaryFormRequest $request) {
+        $year = $request->get('year');
+        $month = $request->get('month');
+
+        $employees = User::whereHas('roles', fn($q) => $q->whereIn('title', config('roles.monthly')))->get();
+        $oldData = Salary::with('user', fn($q) => $q->whereIn('id', $employees->pluck('id')))->whereMonth('from', '=', $month)->whereMonth('to', '=', $month)->get();
+
+        return view('admin.salaries.form.monthly', compact(
+            'year', 'month', 'employees', 'oldData'
+        ));
+
+    }
+
+    public function weeklySalaryProcess(WeeklySalaryProcessRequest $request) {
+        dd($request->all());
+    }
+
+    public function monthlySalaryProcess(MonthlySalaryProcessRequest $request) {
+
     }
 }
