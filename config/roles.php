d218a8518d58d01feb67c2821cc40650c141b774
Sayyid Yofa <sayyidyofa@gmail.com>
2021-01-01 17:22:23 +0700

    Update roles config

--- config/roles.php
+++ config/roles.php
@@ -5,6 +5,7 @@ return [
         'Kepala Bagian Las',
         'Kepala Bagian Potong',
         'Kepala Bagian Hasil Produksi dan Perlengkapan',
+        'Operator Las',
         'Operator Mesin Potong',
         'Operator Mesin Tarek',
         'Operator Bengkokkan',
@@ -14,7 +15,8 @@ return [
         'Kepala Produksi',
         'Pengawas Umum',
         'Wakil Kepala Produksi',
-        'Adm. dan Keuangan'
+        'Adm. dan Keuangan',
+        'Konsultan Akuntansi'
     ],
     'welder' => 'Operator Las',
     'driver' => 'Supir',
